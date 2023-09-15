55f3894e753c92382e38b53a912b7c17afd5d29f
anvita-kumar30 <26528533-anvita-kumar30@users.noreply.replit.com>
2023-09-15 15:42:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+fs.copyFileSync("first.txt", "sec.txt");
+
+// const pi = 3.14;
+
+
+// const supervillains = require("supervillains");
+
+// console.log(supervillains.random());
