39dbb10a59a98933cfbc6979f5332b5f169604a2
anvita-kumar30 <109106936+anvita-kumar30@users.noreply.github.com>
2023-09-15 15:48:49 +0000

    init

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 
 fs.copyFileSync("first.txt", "sec.txt");
 
-// const pi = 3.14;
+const pi = 3.14;
 
 
-// const supervillains = require("supervillains");
+const supervillains = require("supervillains");
 
-// console.log(supervillains.random());
+console.log(supervillains.random());
