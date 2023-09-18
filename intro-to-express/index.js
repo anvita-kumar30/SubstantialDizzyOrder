b7899b37e293e6c80a9c217022351b31d9aca877
anvita-kumar30 <109106936+anvita-kumar30@users.noreply.github.com>
2023-09-18 10:51:19 +0000

    init

--- intro-to-express/index.js
+++ intro-to-express/index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get("/", function(req, res) {
-  res.send("Hello World");
+  res.sendFile(__dirname + "/calci.html");
+  //console.log(__dirname);
+});
+
+app.post("/", function(req, res) {
+  //res.send("We have received your post request");
+  var num1 = Number(req.body.n1);
+  var num2 = Number(req.body.n2);
+  var result = num1 + num2;
+  res.send("This is your magic result: " + result);
+});
+
+app.get("/portfolio", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+  //console.log(__dirname);
 });
 
 app.get("/about", function(req, res) {
